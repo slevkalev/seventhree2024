b2a1919d79aa73570e9d40b92ce7fd9fb2983893
Jim Snelgrove <60110040+slevkalev@users.noreply.github.com>
2024-11-26 22:36:54 -0500

    update home page
    thanksgiving

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,52 @@
 
     <section>
         <h3>News</h3>
-        <p>Week 1 - NFL 2024 Regular Season</p>
-        <p></p>
+        <p>Week 13 - NFL 2024 Regular Season</p>
+        <h5>Thanksgiving Day Schedule</h5>
+        <div class="game-team">
+            <span class="time">12:30 PM</span>
+            <div class="game-team">
+                <img src="./images/logos/bears.png" alt="bears.png" width="40" height="40">
+                <span>Chicago Bears</span>
+                <span>at</span>
+                <img src="./images/logos/lions.png" alt="lions.png" width="40" height="40">
+                <span>Detroit Lions</span>
+            </div>
+        </div>
+
+        <div class="game-team m-ba">
+            <span class="time">4:30 PM</span>
+            <div class="game-team">
+                <img src="./images/logos/giants.png" alt="giants.png" width="40" height="40">
+                <span>NY Giants</span>
+                <span>at</span>
+                <img src="./images/logos/cowboys.png" alt="cowboys.png" width="40" height="40">
+                <span>Dallas Cowboys</span>
+            </div>
+        </div>
+
+        <div class="game-team m-ba">
+            <span class="time">8:20 PM</span>
+            <div class="game-team">
+                <img src="./images/logos/dolphins.png" alt="dolphins.png" width="40" height="40">
+                <span>Miami Dolphins</span>
+                <span>at</span>
+                <img src="./images/logos/packers.png" alt="packers.png" width="40" height="40">
+                <span>Green Bay Packers</span>
+            </div>
+        </div>
+        <h5>Black Friday Schedule</h5>
+
+        <div class="game-team m-ba">
+            <span class="time">3:00 PM</span>
+            <div class="game-team">
+                <img src="./images/logos/raiders.png" alt="raiders.png" width="40" height="40">
+                <span>Las Vegas Raiders</span>
+                <span>at</span>
+                <img src="./images/logos/chiefs.png" alt="chiefs.png" width="40" height="40">
+                <span>Kansas City Chiefs</span>
+            </div>
+        </div>
     </section>
 
     <section>
