09a6393a9d50c7713a8ee976c62433d26a800577
Jim Snelgrove <60110040+slevkalev@users.noreply.github.com>
2026-09-23 08:04:53 -0400

    bigboard show.blade updated
    updated

--- resources/views/football/bigboard/show.blade.php
+++ resources/views/football/bigboard/show.blade.php
@@ -158,7 +158,7 @@
             usersWithTotal.forEach(user=>{
                 let userFlag = user.id === currentUser.id? "current-user" : ""
                 let aiFlag = user.id === 75? "ai-user" : ""
-                leftData += `<div class="row ${userFlag}">
+                leftData += `<div class="row ${userFlag} ${aiFlag}">
                         <span class="data-name-span">${user.first_name.charAt(0)} ${user.last_name}</span>
                         <span class="data-total-span">${user.total}</span>
                         <span class="data-missed-span">${user.missed}</span>
